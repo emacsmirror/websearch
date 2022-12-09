417cab70c3712ca89595fbb575a65a9f760ae67f
Maciej Barć <xgqt@gentoo.org>
2022-12-09 02:20:36 +0100

    extras: add gentoo with 50websearch-gentoo.el
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ extras/gentoo/50websearch-gentoo.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'websearch "websearch"
+  "Query search engines with a METHOD-NAME." t)
+(autoload 'websearch-mode "websearch-mode"
+  "Search engine minor mode." t)
