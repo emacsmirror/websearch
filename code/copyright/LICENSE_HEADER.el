4e99ce8acbe8b7e23737f63b0344363d9581926b
Maciej Barć <xgqt@gentoo.org>
2025-01-06 16:56:40 +0100

    docs(copyright): add LICENSE_HEADER.el
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ code/copyright/LICENSE_HEADER.el
@@ -0,0 +1,16 @@
+;; This file is part of xgqt-elisp-lib-websearch - query search engines from Emacs.
+;; Copyright (c) 2022-2025, Maciej Barć <xgqt@xgqt.org>
+;; Licensed under the GNU GPL v2 License
+;;
+;; xgqt-elisp-lib-websearch is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; xgqt-elisp-lib-websearch is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with xgqt-elisp-lib-websearch.  If not, see <https://www.gnu.org/licenses/>.
